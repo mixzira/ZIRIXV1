50483b486f1ac5ff2985af6dc5547347df773e0b
Mateus Magalhaes <mixzira@outlook.com.br>
2020-06-17 15:55:24 -0300

    Scripts Zirix 1.4

---
+++ resources/[emp]/emp_taxista/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'server.lua'
+}
+
+files {
+	'html/ui.html',
+	'html/taximeter.ttf',
+	'html/cursor.png',
+	'html/styles.css',
+	'html/scripts.js',
+	'html/debounce.min.js'
+}
+
+
