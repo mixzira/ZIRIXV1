154a5e83833bdcb3bb8848fb817be5e6b879dbb5
Mateus Magalhaes <mixzira@outlook.com.br>
2020-06-17 15:30:28 -0300

    Inicializador da base;

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo ===-------------------------------===
+echo     Vrpex Zirix 1.4
+echo     Developed by: Ziraflix
+echo     Discord: discord.gg/6p3M3Cz
+echo     Contato: contato@ziraflix.com   
+echo ===-------------------------------===
+
+pause
+start ..\build\FXServer.exe +exec zirix.cfg +set onesync_enableInfinity 1
+exit
